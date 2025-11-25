1112de3c45a8542b856075b47c45dda6e9676574
engineeritai <engism16@outlook.com>
2025-11-25 22:09:49 +0300

    slight modification of landing page content

--- app/page.tsx
+++ app/page.tsx
@@ -635,9 +635,9 @@ export default function Page() {
                 }}
               >
                 A digital AI-powered platform providing engineering intelligence services, 
-                including automated engineering analysis, data processing, software development, 
+                including automated engineering analysis, data processing, 
                 remote technical consulting, and operating an interactive system that utilizes AI technologies 
-                to analyze documents, drawings, and enhance engineering efficiency.
+                to analyze documents, drawings, and enhance engineering experience.
               </div>
             </div>
             <button
